d888a205d58357da9641b1cbab748bac7a572d46
Natali <n7483205@ya.ru>
2022-10-09 22:45:04 +0700

    second digit of three-digit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿/* принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа:
+456 -> 5,   782 -> 8,   918 -> 1 */
+
+Console.WriteLine("Введите трёхзначное число.");
+int Number = int.Parse(Console.ReadLine()!);
+
+int num1 = Number / 10;
+int num2 = num1 % 10;
+
+Console.WriteLine(num2);
